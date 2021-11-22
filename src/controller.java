5a3259843c92fd93b43602accc69de9ee3daa193
Bruger <mikk00j3@stud.kea.dk>
2021-11-22 10:45:11 +0100

    make new member

--- src/controller.java
+++ src/controller.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class controller {
+    Scanner sc = new Scanner(System.in);
+    public void createNewMamber(){
+        System.out.println("indtast brugerens navn: ");
+        String name = sc.nextLine();
+        System.out.println("indtast alderen på brugeren: ");
+        int age = sc.nextInt();
+        System.out.println("indtast activityformen på brugeren: ");
+        String activityForm = sc.nextLine();
+        System.out.println("indtast activitylevelet på brugeren: ");
+        String activityLevel = sc.nextLine();
+        Member m = new Member(name,age,activityForm,activityLevel);
+    }
 }
