aebea9aba4034afa7f39e05f7bcbc59b0fab6574
johanne <johanne_riis@hotmail.com>
2021-11-22 11:06:06 +0100

    Splittet subcriptionFee op i flere metoder

--- src/accounting/SubscriptionFee.java
+++ src/accounting/SubscriptionFee.java
@@ -1,42 +1,45 @@
 package accounting;
+import Member;
 
 public class SubscriptionFee {
     private double below18Fee = 1000;
-    private  double above18Fee= 1600;
-    private double passiveFee=500;
+    private double above18Fee = 1600;
+    private double passiveFee = 500;
     private double seniorFeeDiscount = 0.75;
 
 
-
-    public double getsubscriptionFee() {
-        double subscribtionFee;
+    public double getSubscriptionFee() {
+        double subscriptionFee;
         if (!isMemberActive()) {
-            subscribtionFee = passiveFee;
+            subscriptionFee = passiveFee;
         } else {
-            subscribtionFee = calculateSubFee();
+            subscriptionFee = calculateSubFee();
         }
-        return subscribtionFee;
+        return subscriptionFee;
 
     }
 
     private boolean isMemberActive() {
-        return member.getActivityLevel;
+        boolean active;
+        if (getActivityLevel.equals("active")) {
+            active = true;
+        } else {
+            active = false;
+        }
+        return active;
     }
 
     private double calculateSubFee() {
-        double subscribtionFee;
+        double calculatedSubscriptionFee;
         int age = member.getAge;
-          if(age<=18){
-                subscribtionFee=below18Fee;}
-            else if ((age>18) && (age<60)){
-                subscribtionFee= above18Fee;}
-            else{
-                subscribtionFee=above18Fee*seniorFeeDiscount;
-            }
-            return subscribtionFee;
+        if (age < 18) {
+            calculatedSubscriptionFee = below18Fee;
+        } else if (age < 60) {
+            calculatedSubscriptionFee = above18Fee;
+        } else {
+            calculatedSubscriptionFee = above18Fee * seniorFeeDiscount;
         }
-        }
+        return calculatedSubscriptionFee;
     }
-
-
 }
+
