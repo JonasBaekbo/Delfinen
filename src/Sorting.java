0e74c5b84ab723cddf3a9072f6c9d2859fbf6f50
Bruger <mikk00j3@stud.kea.dk>
2021-11-22 10:12:38 +0100

    sorter

---
+++ src/Sorting.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+
+public class Sorting implements Comparator<Member> {
+    private String type;
+
+    public Sorting(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public int compare(Member o1, Member o2) {
+        int resultat = 0;
+        if (type.equals("age")) {
+            resultat = Integer.compare(o1.getAge(), o2.getAge());
+        } else if (type.equals("name")) {
+            resultat = o1.getName().compareTo(o2.getName());
+        } else if (type.equals("activityForm")) {
+            resultat = o1.getActivityForm().compareTo(o2.getActivityForm());
+        } else if (type.equals("activityLevel")) {
+            resultat = o1.getActivityLevel().compareTo(o2.getActivityLevel());
+        }
+        return resultat;
+    }
+}
