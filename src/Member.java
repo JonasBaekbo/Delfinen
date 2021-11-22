5555cf11021b483296031548a8b533a7e8179409
adambl99 <adam6925@stud.kea.dk>
2021-11-22 09:59:47 +0100

    Member with Get and Setters

--- src/Member.java
+++ src/Member.java
@@ -1,11 +1,13 @@
-public class CreateMember {
+
+
+public class Member {
     String name;
     int age;
     String activityForm; //e.g. junior- eller seniorsvømmer, motionist eller konkurrencesvømmer.
     String activityLevel; //e.g. Aktivt eller passivt medlem.
 
 
-    public CreateMember(String name, int age, String activityForm, String activityLevel){
+    public Member(String name, int age, String activityForm, String activityLevel){
         this.name = name;
         this.age = age;
         this.activityForm = activityForm;
