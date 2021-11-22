a114fe69385ce1d3272bf2cb88f8ad85ca85049b
adambl99 <adam6925@stud.kea.dk>
2021-11-22 09:32:15 +0100

    CreateMember

---
+++ src/CreateMember.java
@@ -0,0 +1,17 @@
+public class CreateMember {
+    String name;
+    int age;
+    String activityForm; //e.g. junior- eller seniorsvømmer, motionist eller konkurrencesvømmer.
+    String activityLevel; //e.g. Aktivt eller passivt medlem.
+
+
+    public CreateMember(String name, int age, String activityForm, String activityLevel){
+        this.name = name;
+        this.age = age;
+        this.activityForm = activityForm;
+        this.activityLevel = activityLevel;
+    }
+
+
+
+}
