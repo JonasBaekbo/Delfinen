0dc856ea938326fbaf19c3bc8868404da8235391
adambl99 <adam6925@stud.kea.dk>
2021-11-22 09:48:53 +0100

    CreateMember with Get and Setters

--- src/CreateMember.java
+++ src/CreateMember.java
@@ -12,6 +12,35 @@ public class CreateMember {
         this.activityLevel = activityLevel;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getActivityForm() {
+        return activityForm;
+    }
+
+    public void setActivityForm(String activityForm) {
+        this.activityForm = activityForm;
+    }
+
+    public String getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(String activityLevel) {
+        this.activityLevel = activityLevel;
+    }
 }
