2f3fae6f31c50bf8ed15b18e748a7699a4e02cca
Bruger <mikk00j3@stud.kea.dk>
2021-11-24 11:19:40 +0100

    better userinterface

--- src/Domain/Controller.java
+++ src/Domain/Controller.java
@@ -7,7 +7,6 @@ import ui.UserInterface;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Objects;
-import java.util.Scanner;
 
 public class Controller {
     FileHandler files = new FileHandler();
@@ -17,28 +16,54 @@ public class Controller {
     private UserInterface ui = new UserInterface();
     private SubscriptionFee subFee = new SubscriptionFee();
     private ArrayList<Member> members = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    //private String svømmediciplin;
-    private Member m;
 
     public void start() throws FileNotFoundException {
         ui.printMessage("Velkommen til Delfinen");
         ui.printMessage("-----------------------");
+        maineMenu();
+    }
+
+    public void maineMenu() throws FileNotFoundException {
         while (isRunning) {
-            ui.menu();
+            ui.Mainemenu();
             switch (ui.userInput()) {
                 case "0" -> exit();
-                case "1" -> createNewMember();
-                case "2" -> createCoach();
-                case "3" -> addTimeAndDateTooMember();
-                case "4" -> bestPracticeTime();
-                case "5" -> tournamentsResults();
-                case "6" -> showTop5Swimmers(files.getAllMembers(MEMBER_FILE));
-                case "7" -> chargeSubscriptionFee();
-                case "8" -> markAsPaid();
-                case "9" -> calculateExpectedSubFeeTotal();
-                case "10"-> sowMissingPayments();
+                case "1" -> CEOManu();
+                case "2" -> coachMenu();
+                case "3" -> treasurerMenu();
+
             }
+        }
+    }
+
+    public void CEOManu() throws FileNotFoundException {
+        ui.menuCEO();
+        switch (ui.userInput()) {
+            case "1" -> createNewMember();
+            case "2" -> createCoach();
+            case "0" -> backTooMainMenu();
+        }
+    }
+
+    public void coachMenu() throws FileNotFoundException {
+        ui.menuCoach();
+        switch (ui.userInput()) {
+            case "1" -> addTimeAndDateTooMember();
+            case "2" -> bestPracticeTime();
+            case "3" -> tournamentsResults();
+            case "4" -> showTop5Swimmers(files.getAllMembers(MEMBER_FILE));
+            case "0" -> backTooMainMenu();
+        }
+    }
+
+    public void treasurerMenu() throws FileNotFoundException {
+        ui.menuTreasurer();
+        switch (ui.userInput()) {
+            case "1" -> chargeSubscriptionFee();
+            case "2" -> markAsPaid();
+            case "3" -> calculateExpectedSubFeeTotal();
+            case "4" -> sowMissingPayments();
+            case "0" -> backTooMainMenu();
         }
     }
 
@@ -244,5 +269,9 @@ private void markAsPaid(){
 
     public void exit() {
         isRunning = false;
+    }
+
+    public void backTooMainMenu() throws FileNotFoundException {
+        maineMenu();
     }
 }
