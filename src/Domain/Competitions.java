f52a367fc9d27528d81e245e881b26e809bf36fd
Bruger <mikk00j3@stud.kea.dk>
2021-11-24 12:30:58 +0100

    added Competitions class

---
+++ src/Domain/Competitions.java
@@ -0,0 +1,13 @@
+package Domain;
+
+public class Competitions {
+
+    private String convention;
+    private int place;
+    private int time;
+    public Competitions(String convention, int place, int time){
+        this.convention = convention;
+        this.place = place;
+        this.time = time;
+    }
+}
